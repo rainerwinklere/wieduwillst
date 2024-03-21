270cfd0848d9e3171a203abbe4ef415859edda6b
rainerwinklere <164182962+rainerwinklere@users.noreply.github.com>
2024-03-21 14:06:55 +0100

    a
    a

---
+++ #List.py
@@ -0,0 +1,8 @@
+#List 
+
+a=[4,"hello",123, True]
+
+#print(a[0])
+
+for i in a: 
+        print(i)
---
+++ hehehhoho.py
@@ -0,0 +1,9 @@
+#While schleife 
+a = 1 
+
+while a < 10:
+    print (f"der wert von a ist {a}")
+    a = a + 1 
+    
+for i in range(10):
+    print(f"der wert von i ist {i}")
